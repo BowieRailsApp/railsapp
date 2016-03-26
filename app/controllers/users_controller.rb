f5e24a55742b94b0180bfecdfec15e2071c70c0c
Sara Horowitz <sslhorowitz@gmail.com>
2016-03-26 17:44:16 -0400

    initial set-up of database, re-configure of models

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -29,7 +29,7 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:username, :password, :password_confirmation)
+    params.require(:user).permit(:username, :password, :password_confirmation, :employee_type)
   end
 
 end
