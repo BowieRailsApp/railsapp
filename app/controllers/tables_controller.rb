51afaed583b272d4bc47d0ef2ff04b7b6005eb9f
Sara Horowitz <sslhorowitz@gmail.com>
2016-03-28 16:22:46 -0400

    added manage tables to nav

--- app/controllers/tables_controller.rb
+++ app/controllers/tables_controller.rb
@@ -1,7 +1,46 @@
 class TablesController  < ApplicationController
 
-  def index
-    @tables = Table.all
-  end
+ def index
+   @tables = Table.all
+ end
+ def show
+   @table = Table.find(params[:id])
+ end
 
-end
+ def new
+   @table = Table.new
+ end
+
+ def create
+   @table = Table.new(table_params)
+   if @table.save
+   redirect_to tables_path
+   else
+   redirect_to new_table_path
+   end
+ end
+
+ def edit
+   @table = Table.find(params[:id])
+ end
+
+ def update
+   @table = Table.find(params[:id])
+   @table.update(table_params)
+   redirect_to tables_path
+ end
+
+ def destroy
+   @table = Table.find(params[:id])
+   @table.destroy
+   redirect_to tables_path
+ end
+
+ private
+
+ def table_params
+   params.require(:table).permit(:guests)
+ end
+
+
+end 
