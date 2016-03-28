f4dc89ab0827d3fafc9655a6fe5bbb29cf5c2287
Elton Cheng <eltoncheng86@gmail.com>
2016-03-28 11:23:36 -0400

    need dan's code

--- db/migrate/20160326233205_create_orders.rb
+++ db/migrate/20160326233205_create_orders.rb
@@ -3,6 +3,7 @@ class CreateOrders < ActiveRecord::Migration
     create_table :orders do |t|
       t.references :table
       t.boolean :cooked, :default => false
+      t.timestamps
     end
   end
 end
