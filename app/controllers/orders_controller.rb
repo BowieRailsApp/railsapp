439d85cad3805e8992afc9cbefda68cfc084a367
Razaik Boparai <razaikb@gmail.com>
2016-03-27 22:45:02 -0400

    able to show all orders

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -15,9 +15,17 @@ class OrdersController < ApplicationController
   end
 
   def create
-    @order = Order.new(order_params)
 
+    @order = Order.new(order_params)
+    # dropping the first element of the array as it is always "" for some reason.
+    foodarr = params["fooditem_order"]["fooditem_id"].drop(1)
     if @order.save
+      # looping over food arr and inserting the values in fooditems_orders join table
+      foodarr.each { |id|
+        fooditem = Fooditem.find(id)
+        order = Order.find(@order.id)
+        order.fooditems << fooditem
+      }
       redirect_to orders_path
     else
       redirect_to new_order_path
@@ -27,7 +35,7 @@ class OrdersController < ApplicationController
   private
 
   def order_params
-    params.require(:order).permit(:table_id, :cooked)
+    params.require(:order).permit(:table_id, :fooditem_id , :cooked)
   end
 
 end
