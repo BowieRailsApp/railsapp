39f1d28a762e72cf323f48de71b28e26316d61e9
Sara Horowitz <sslhorowitz@gmail.com>
2016-03-28 11:29:18 -0400

    updated order-controller

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -15,21 +15,40 @@ class OrdersController < ApplicationController
   end
 
   def create
-
     @order = Order.new(order_params)
     # dropping the first element of the array as it is always "" for some reason.
-    foodarr = params["fooditem_order"]["fooditem_id"].drop(1)
+    foodarr = params["order"]["fooditem_order"]["fooditem_id"].drop(1)
+
     if @order.save
       # looping over food arr and inserting the values in fooditems_orders join table
       foodarr.each { |id|
         fooditem = Fooditem.find(id)
-        order = Order.find(@order.id)
-        order.fooditems << fooditem
+        # order = Order.find(@order.id)
+        @order.fooditems << fooditem
       }
       redirect_to orders_path
     else
       redirect_to new_order_path
     end
+  end
+
+  def edit
+    @order = Order.find(params[:id])
+    @tables = Table.all
+    @fooditems = Fooditem.all
+  end
+
+  def update
+    @order = Order.find(params[:id])
+    @order.update(order_params)
+    redirect_to orders_path
+  end
+
+  def destroy
+    # if the order associated with the food item is cooked then delete the food item
+    @fooditem = Fooditem.find(params[:id])
+    @fooditem.destroy
+    redirect_to fooditems_path
   end
 
   private
--- app/models/fooditem.rb
+++ app/models/fooditem.rb
@@ -1,6 +1,6 @@
 class Fooditem < ActiveRecord::Base
 
-  has_and_belongs_to_many :orders
+  has_and_belongs_to_many :orders, through: :fooditem_orders
 
   validates :itemname, presence: true, uniqueness: true
   validates :cuisine, presence: true
